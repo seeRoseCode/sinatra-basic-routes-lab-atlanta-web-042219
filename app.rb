bc3233564836866e22e4265cb68d415d6982ec48
seerosecode <keshalakemusic@gmail.com>
2019-05-13 10:06:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+      get '/' do
+        "Hello, World!"
+      end
+
+      get '/name' do
+        "My name is Rose"
+      end
+
+      get '/hometown' do
+        "My hometown is in Los Angeles, Ca"
+      end
+
+      get '/favorite-song' do
+        "My favorite song is Won't He Do It by Koryn Hawthorne"
+      end
+
+  end
